20092b101a9e764c76cb88327e6d81f4ef18f830
0752189 <0752189@KELCE-ACIS16-P>
2017-10-09 17:40:02 -0500

    User Class added

---
+++ PittGo/PittGo/Classes/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PittGo.Classes
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string EmailAddress { get; set; }
+        public int Password { get; set; }
+
+
+
+
+
+
+
+
+ 
+
+    }
+}
