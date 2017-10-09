56f0aeb7ce704eb400bb5bbabd0d2c2369426449
0770246 <0770246@KELCE-ACIS17-P>
2017-10-09 17:48:22 -0500

    institution page created

---
+++ PittGo/PittGo/Institutions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PittGo
+{
+    class Institutions
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Groceryoptions { get; set; }
+        public string Gatheringoptions { get; set; }
+        public string restaurants { get; set; }
+        public string Paybills { get; set; }
+        public string location { get; set; }
+
+    }
+}
