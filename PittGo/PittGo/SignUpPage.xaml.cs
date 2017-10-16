b4646358324665930be9fbbf0cf346ce781ddccf
0752189 <0752189@KELCE-ACIS17-P>
2017-10-16 18:11:02 -0500

    Main Page Finished

---
+++ PittGo/PittGo/SignUpPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PittGo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        private void countryButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new country());
+        }
+    }
+}
