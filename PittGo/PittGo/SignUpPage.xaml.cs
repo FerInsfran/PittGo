3793c646a955f98afd3e33c0460016104b4150e5
0781179 <0781179@KELCE-ACIS14-P>
2017-11-20 18:15:20 -0600

    new

--- PittGo/PittGo/SignUpPage.xaml.cs
+++ PittGo/PittGo/SignUpPage.xaml.cs
@@ -17,24 +17,19 @@ namespace PittGo
         {
             InitializeComponent();
 
-            if (Settings.UserData != null)
-            {
-                this.email.text = Settings.UserData.EmailAddress;
-                this.password.text = Settings.UserData.Password;
-            }
+            
+            
         }
 
         private void UpdateButton_Clicked(object sender, EventArgs e)
         {
-            User userToSave = new User();
-            userToSave.EmailAddress = this.email.Text;
-            userToSave.Password = this.password.Text;
+         
 
             // Save User to Azure
 
 
 
-            Settings.UserData = userToSave;
+            
 
             //GlobalConfig.LoggedInUser = userToSave;
 
--- PittGo/PittGo/country.xaml.cs
+++ PittGo/PittGo/country.xaml.cs
@@ -19,22 +19,22 @@ namespace PittGo
 
         private void TapGestureRecognizer_Tapped_INDIA(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new institutionpage("INDIA"));
+            Navigation.PushAsync(new institutionpage("C1D9BBC5-BFAD-4A06-BF4A-37D588A3E154"));
         }
 
         private void TapGestureRecognizer_Tapped_PARAGUAY(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new institutionpage("PARAGUAY"));
+            Navigation.PushAsync(new institutionpage("85FAFB94-775C-4121-B9EF-F9457C34D571"));
         }
 
         private void TapGestureRecognizer_Tapped_CHINA(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new institutionpage("CHINA"));
+            Navigation.PushAsync(new institutionpage("670307E1-7609-4C5B-AE43-5532E3BC64DB"));
         }
 
         private void TapGestureRecognizer_Tapped_SAUDIARABIA(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new institutionpage("SAUDIARABIA"));
+            Navigation.PushAsync(new institutionpage("F9D1B7E3-683B-4351-B8F5-0975A17EBF9E"));
         }
     }
 }
